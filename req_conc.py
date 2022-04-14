dec499cd6f4e66732a71ad13cae9d9c6fda6898b
runiorr <joseluisrjunior@gmail.com>
2022-04-13 21:22:16 -0300

    refactor: list compreension

--- req_conc.py
+++ req_conc.py
@@ -3,32 +3,23 @@ import threading
 from utils.info import time_thread
 
 @time_thread
-def url_parser(url: str):
+def get_url(url: str):
     resp = requests.get(url)
     try:
-        print(resp.json())
+        print(f"Response: {resp.json()}")
     except Exception as err:
-        print(resp.text)
+        print(f"Response: {resp.text}")
 
-def thread_exec(urls: list):
-    th_count = 0
-    for url in urls:
-        th = threading.Thread(target=url_parser, args=([url]))
-        th.start()
-        th_count+=1
-        
-    print(f'{th_count} Threads created and closed\n')
+def thread_exec(url):
+    th = threading.Thread(target=get_url, args=([url]))
+    th.start()
 
-# @time_thread
 def main():
     # Run each request on a thread
-
     urls = ['https://httpbin.org/headers', 'https://httpbin.org/get']
-    thread_exec(urls)
-
+    [thread_exec(url) for url in urls]
 
     # Run each request on same thread
-
     # for url in urls:
     #     url_parser(url)
 
