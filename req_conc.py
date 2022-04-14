dab909d4db30bed02391e0aeabb5764ad365a5f5
runiorr <joseluisrjunior@gmail.com>
2022-04-13 22:07:46 -0300

    feat: explicit method

--- req_conc.py
+++ req_conc.py
@@ -5,26 +5,23 @@ from utils.helper import time_elapsed, type_check, func_info
 
 @time_elapsed
 @func_info
-def get_url(url: str):
+@type_check(str, str)
+def url_parser(url: str, method: str):
     resp = requests.get(url)
-    try:
-        print(f"\nResponse: {resp.json()}")
-    except JSONDecodeError:
-        print(f"\nResponse: {resp.text}")
+    if method == "GET":
+        try:
+            print(f"\nResponse: {resp.json()}")
+        except JSONDecodeError:
+            print(f"\nResponse: {resp.text}")
 
-@type_check((str,list))
-def thread_exec(url):
-    th = threading.Thread(target=get_url, args=([url]))
+@type_check(str)
+def thread_exec(url: str):
+    th = threading.Thread(target=url_parser, args=([url, "GET"]))
     th.start()
 
 def main():
-    # Run each request on a thread
     urls = ['https://httpbin.org/headers', 'https://httpbin.org/get']
     [thread_exec(url) for url in urls]
-
-    # Run each request on same thread
-    # for url in urls:
-    #     url_parser(url)
 
 if __name__ == "__main__":
     main()
